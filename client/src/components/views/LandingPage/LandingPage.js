ad8ff73a6fa94f717b5c1c3debf9eb05aa31bea9
Hwang <hth950@gmail.com>
2021-06-14 20:33:59 +0900

    Checkbox, Radiobox

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,33 +1,75 @@
 import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
-import { Icon, Col, Card, Row } from "antd";
+import { Icon, Col, Card, Row, Carousel } from "antd";
 import Meta from 'antd/lib/card/Meta'
+import ImageSlider from '../../utils/ImageSlider';
+import Checkbox from './Sections/CheckBox';
+import {continents, price} from './Sections/Datas';
+import Radiobox from './Sections/RadioBox';
 
 function LandingPage() {
 
     const [Products, setProducts] = useState([])
+    const [Skip, setSkip] = useState(0)
+    const [Limit, setLimit] = useState(8)
+    const [PostSize, setPostSize] = useState(0)
+    const [Filters, setFilters] = useState({
+        continents : [],
+        price : []
+    })
 
     useEffect(() => {
 
-        axios.post('api/product/products',)
+        let body = {
+            skip: Skip,
+            limit: Limit
+        }
+
+        getProducts(body)
+
+    }, [])
+
+    const getProducts = (body) => {
+        axios.post('api/product/products', body)
             .then(response => {
                 if (response.data.success) {
-                    console.log(response.data)
-                    setProducts(response.data.productInfo)
+                    //console.log(response.data)
+                    if (body.loadMore) {
+                        setProducts([...Products, ...response.data.productInfo])
+                    } else {
+                        setProducts(response.data.productInfo)
+                    }
+                    setPostSize(response.data.postSize)
+
                 } else {
                     alert("상품들을 가져오는데 실패했습니다.")
                 }
             })
+    }
 
-    }, [])
+    const loadMoreHanlder = () => {
+
+        let skip = Skip + Limit
+
+        let body = {
+            skip: skip,
+            limit: Limit,
+            loadMore: true
+        }
+
+        getProducts(body)
+        setSkip(skip)
+
+    }
 
     const renderCards = Products.map((product, index) => {
 
-        console.log('product', product)
+        //console.log('product', product)
         return <Col lg={6} md={8} xs={24} key={index}>
-            <Card          
-                cover={<img style={{ width:'100%', maxHeight:'150px'}} src={`http://localhost:5000/${product.images[0]}`} />}
+            <Card
+                //<img style={{ width:'100%', maxHeight:'150px'}} src={`http://localhost:5000/${product.images[0]}`} /></Col>
+                cover={<ImageSlider images={product.images} />}
             >
                 <Meta
                     title={product.title}
@@ -37,6 +79,50 @@ function LandingPage() {
         </Col>
     })
 
+    const showFilterdResults = (filters) =>{
+
+        let body = {
+            skip: 0,
+            limit: Limit,
+            filters : filters
+        }
+
+        getProducts(body)
+        setSkip(0)
+
+    }
+
+    const handlePrice= (value) =>{
+
+        const data = price;
+        let array = [];
+
+        for (let key in data){
+            if(data[key]._id === parseInt(value, 10)){
+                array = data[key].array
+            }
+        }
+
+        return array;
+    }
+
+    const handleFilters = (filters, category) => {
+
+        const newFilters ={...Filters}
+
+        newFilters[category] = filters
+
+        console.log('filters', filters)
+
+        if(category ==='price'){
+            let priceValues = handlePrice(filters)
+            newFilters[category] = priceValues
+        }
+
+        showFilterdResults(newFilters)
+        setFilters(newFilters)
+    }
+
 
     return (
         <div style={{ width: "75%", margin: '3rem auto' }}>
@@ -45,20 +131,41 @@ function LandingPage() {
             </div>
 
             {/* Filter */}
+        <Row gutter = {[16,16]}>
+            <Col lg={12} xs={24}>
+            
+            {/*CheckBox*/}
+            <Checkbox list={continents} handleFilters={filters => handleFilters(filters,"continents")}/>
+            </Col >
+
+            <Col lg={12} xs={24}>
+            {/*RadioBox*/}
+            <Radiobox list={price} handleFilters={filters => handleFilters(filters,"price")}>
+
+            </Radiobox>
+
+            </Col>
+        </Row>
+            
+
 
             {/* Search */}
 
             {/* Cards */}
-            <Row gutter={[16,16]}>
+
+
+            <Row gutter={[16, 16]}>
                 {renderCards}
             </Row>
 
+            <br />
 
-            <div style={{ displat: 'flex', justifyContent: 'center' }}>
-                <button>더보기</button>
-            </div>
-
-        </div>
+            {PostSize >= Limit &&
+                < div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button onClick={loadMoreHanlder}>더보기</button>
+                </div>
+            }
+        </div >
     )
 }
 
--- server/routes/product.js
+++ server/routes/product.js
@@ -51,11 +51,42 @@ router.post('/products', (req, res) => {
 
     // product collection 에 들어있는 모든 상품 정보를 가져오기
 
-    Product.find()
+    let limit = req.body.limit ? parseInt(req.body.limit) : 50;
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+    let findArgs = {};
+
+    // 다시한번 보기
+    for(let key in req.body.filters){
+        if(req.body.filters[key].length > 0){
+
+            //console.log('key', key)
+            if(key ==='price'){
+                findArgs[key] = {
+                    // greater than equal 
+                    $gte: req.body.filters[key][0],
+                    // less than equal
+                    $lte: req.body.filters[key][1]
+                }
+            }else{
+                findArgs[key] = req.body.filters[key];
+            }
+
+        }
+    }
+
+    console.log('findArgs', findArgs)
+
+    Product.find(findArgs)
     .populate("writer")
+    .skip(skip)
+    .limit(limit)
     .exec((err, productInfo) =>{
         if (err) return res.status(400).json({success : false, err})
-        return res.status(200).json({success : true, productInfo})
+        return res.status(200).json({
+            success : true, productInfo,
+            postSize : productInfo.length
+        })
     })
 
 })
